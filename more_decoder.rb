0ad990595db0854468b952d64955f1ca08236b17
Retky <jose.arredondo.a@outlook.com>
2022-06-08 17:31:57 -0500

    Change 'syntax' for linters

--- more_decoder.rb
+++ more_decoder.rb
@@ -1,15 +1,20 @@
 def decode_char(char)
-    morseCode = {
-        '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f', '--.' => 'g', '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n', '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't', '..-' => 'u', '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y', '--..' => 'z', '.-.-.-' => '.'
-    }
-    return morseCode[char]
+  morse_code = {
+    '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e',
+    '..-.' => 'f', '--.' => 'g', '....' => 'h', '..' => 'i', '.---' => 'j',
+    '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n', '---' => 'o',
+    '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't',
+    '..-' => 'u', '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y',
+    '--..' => 'z'
+  }
+  morse_code[char]
 end
 
-def decode_word (char)
-    newArray = char.split(' ')
-    emptyString = ''
-    newArray.each do |i|
-        emptyString += decode_char(i)
-    end
-    return emptyString
+def decode_word(char)
+  new_array = char.chars('')
+  empty_string = ''
+  new_array.each do |i|
+    empty_string += decode_char(i)
+  end
+  empty_string
 end
